5d8197d29af8cb07f2ab93c068bcfc2075578392
Nathan Cooper <nac33@students.uwf.edu>
2017-11-02 00:25:06 -0500

    Add Kattis problems

--- Unsolved/src/Kattis/Grid.java
+++ Unsolved/src/Kattis/Grid.java
@@ -1,105 +1,117 @@
+package Kattis;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
 
 /**
- *
- * @author kms118
+ * Created by natha on 11/1/2017.
  */
 public class Grid
 {
 
-    static int min = Integer.MAX_VALUE;
-    
+    static int[][] grid;
     static int[][] dist;
-    
-    public static void main(String[] args) throws Exception
+
+    public static void main(String[] args) throws IOException
     {
-    
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] input = br.readLine().split(" ");
-        
-        int n = Integer.parseInt(input[0]);
-        int m = Integer.parseInt(input[1]);
-        
-        
-        int[][] grid = new int[n][m];
-        boolean[][] visited = new boolean[n][m];
+
+
+        String[] dimensions = br.readLine().split(" ");
+        int n = Integer.parseInt(dimensions[0]);
+        int m = Integer.parseInt(dimensions[1]);
+        grid = new int[n][m];
         dist = new int[n][m];
-        for (int i = 0; i < visited.length; i++)
-        {
-            Arrays.fill(visited[i], false);
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
+
+        for (int i = 0; i < n; i++)
+            Arrays.fill(dist, Integer.MAX_VALUE);
+
         for (int i = 0; i < n; i++)
         {
-        
+
             for (int j = 0; j < m; j++)
             {
-                
-          
-                char num = (char) br.read();
-                String number = "" + num;
-               // System.out.println(number);
-                grid[i][j] = Integer.parseInt(number);
-            
-            }
-            String junk = br.readLine();
-        }
-        
-        //for (int k = 0; k < grid.length; k++)
-          //  System.out.println(Arrays.toString(grid[k]));
-          
-          backtracking(grid, visited, 0, 0, 0);
-          
-          if (min == Integer.MAX_VALUE)
-              System.out.println(-1);
-          else
-            System.out.println(min);
-        
-        
-    
-    }
-    
-    
-    static void backtracking(int[][] grid, boolean[][] visited, int x, int y, int s)
-    {
-    
-        if (y == grid.length - 1 && x == grid[0].length - 1)
-        {
-        
-           // visited[y][x] = true;
-            if (min > s)
-                min = s;
-            return;
-            
-        }
-        
-        if ((y >= grid.length || y < 0) || (x >= grid[0].length || x < 0))
-            return;
-        
-        if (visited[y][x])
-            return;
-        
-        if (grid[y][x] == 0)
-        {
-            visited[y][x] = true;
-            return;
-        }
-        
-        
-        
-        visited[y][x] = true;
-        
-        backtracking(grid, visited, x + grid[y][x], y, s + 1);
-        backtracking(grid, visited, x - grid[y][x], y, s + 1);
-        backtracking(grid, visited, x, y + grid[y][x], s + 1);
-        backtracking(grid, visited, x, y - grid[y][x], s + 1);
 
-    
+                grid[i][j] = br.read() - '0';
+
+            }
+
+            br.read();
+
+        }
+
+//        for (int i = 0; i < n; i++)
+//            System.out.println(Arrays.toString(grid[i]));
+
     }
-    
+
+    static void breadth_first_search()
+    {
+
+        int[] start = {0, 0};
+        Queue<int[]> queue = new PriorityQueue<>();
+
+        queue.offer(start);
+        while(!queue.isEmpty())
+        {
+
+            int[] current = queue.poll();
+
+            if (moveUp(current))
+            {
+                int[] next = {current[0] - grid[current[0]][current[1]], current[1]};
+                if (dist[next[0]][next[1]] > dist[current[0]][current[1]])
+                    dist[next[0]][next[1]] = dist[current[0]][current[1]] + 1;
+            }
+
+        }
+
+    }
+
+    static boolean moveUp(int[] current)
+    {
+
+        if (current[0] - grid[current[0]][current[1]] < 0)
+            return false;
+
+        return true;
+
+    }
+
+    static boolean moveDown(int[] current)
+    {
+
+        if (current[0] + grid[current[0]][current[1]] >= grid.length)
+            return false;
+
+        return true;
+
+    }
+
+    static boolean moveLeft(int[] current)
+    {
+
+        if (current[1] - grid[current[0]][current[1]] < 0)
+            return false;
+
+        return true;
+
+    }
+
+    static boolean moveRight(int[] current)
+    {
+
+        if (current[1] + grid[current[0]][current[1]] >= grid[0].length)
+            return false;
+
+        return true;
+
+    }
+
 }
